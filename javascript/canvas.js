f1cb830e86a2e00b59900eefafa7b43ee6f37b2a
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2023-11-07 15:13:06 -0300

    Agregar expresiones de felicidad al salvarse de una muerte segura
    Acá le cambié los gestos de la carita al adivinar la palabra y encontrarse a salvo

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -68,12 +68,12 @@ function dibujarPiernas(){
 
 function dibujarOjosMuertos(){
     ctx.beginPath();
-    //ojo izquierdo
+    //* ojo izquierdo
     ctx.moveTo(130,135);
     ctx.lineTo(140,150);
     ctx.moveTo(140,135);
     ctx.lineTo(130,150);
-    //ojo derecho
+    //* ojo derecho
     ctx.moveTo(160,135);
     ctx.lineTo(170,150);
     ctx.moveTo(170,135);
@@ -82,4 +82,56 @@ function dibujarOjosMuertos(){
     ctx.lineWidth = 5;
     ctx.stroke();
     ctx.closePath();
+}
+
+function dibujarBocaZigzag() {
+  ctx.beginPath();
+  ctx.moveTo(140, 170); //* Llevo a la posición inicial de la boca
+  ctx.lineTo(143, 175); //* Acá dibujo el primer triángulo
+  ctx.lineTo(146, 170);
+  ctx.lineTo(149, 175); //* Acá dibujo el segundo triángulo
+  ctx.lineTo(152, 170);
+  ctx.lineTo(155, 175); //* Acá dibujo el tercer triángulo
+  ctx.lineTo(158, 170);
+  ctx.lineTo(161, 175); //* Acá dibujo el cuarto triángulo
+  ctx.lineTo(164, 170);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.closePath();
+}
+
+
+function dibujarOjosFelices() {
+  //* Dibujo dos ojitos felices (como 2 círculos azules) en la posición deseada.
+  ctx.beginPath();
+  ctx.arc(140, 145, 5, 0, Math.PI * 2, false);
+  ctx.fillStyle = 'blue';
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.arc(160, 145, 5, 0, Math.PI * 2, false);
+  ctx.fillStyle = 'blue';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function dibujarSonrisaFeliz() {
+  //* Acá dibujé una sonrisa feliz (en forma de arco) en la posición deseada.
+  ctx.beginPath();
+  ctx.arc(150, 165, 15, 0, Math.PI, false);
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'red';
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function dibujarLenguaFeliz() {
+  //* Acá hice la lengua de alegría (lo resolví como un óvalo) en la posición deseada.
+  ctx.beginPath();
+  ctx.ellipse(150, 178, 8, 20, 0, 0, Math.PI, false);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+  ctx.closePath();
 }
